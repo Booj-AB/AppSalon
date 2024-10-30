d8ec56fef903f51b3ce9c70704a47b81a8e1020e
Booj-AB <boudjemlineabdalhadi@gmail.com>
2024-10-30 22:07:40 +0100

    Create src and other Folder

---
+++ src/constants/images.js
@@ -0,0 +1,42 @@
+const avatar = require('../../assets/pageHomeAssets/avatar.png');
+const aboutImg = require('../../assets/pageHomeAssets/aboutImg.png');
+const LOGO = require('../../assets/pageHomeAssets/LOGO.png');
+const logo2 = require('../../assets/pageHomeAssets/logo2.png');
+const logo3 = require('../../assets/pageHomeAssets/logo3.png');
+const logo4 = require('../../assets/pageHomeAssets/logo4.png');
+const service1 = require('../../assets/pageHomeAssets/service1.png');
+const service2 = require('../../assets/pageHomeAssets/service2.png');
+const service3 = require('../../assets/pageHomeAssets/service3.png');
+
+const produit1 = require('../../assets/pageHomeAssets/produit1.png');
+const produit2 = require('../../assets/pageHomeAssets/produit2.png');
+const produit3 = require('../../assets/pageHomeAssets/produit3.png');
+const produit4 = require('../../assets/pageHomeAssets/produit4.png');
+const produit5 = require('../../assets/pageHomeAssets/produit5.png');
+const produit6 = require('../../assets/pageHomeAssets/produit6.png');
+const produit7 = require('../../assets/pageHomeAssets/produit7.png');
+
+
+
+
+
+
+export default {
+
+avatar,
+aboutImg,
+LOGO,
+logo2,
+logo3,
+logo4,
+service1,
+service2,
+service3,
+produit1,
+produit2,
+produit3,
+produit4,
+produit5,
+produit6,
+produit7,
+};
